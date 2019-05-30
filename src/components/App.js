0059c046c9be34e60733a9f622476d3b85b01daa
AbdElRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2019-05-30 14:07:04 +0200

    adding Router and new tweet and tweet page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import Nav from './Nav'
+import TweetPage from './TweetPage'
+
 
 class App extends Component {
   componentDidMount() {
@@ -10,12 +15,21 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <Router>
+        <Fragment>
           <LoadingBar />
-          {this.props.loading === true
-          ? null
-          : <Dashboard />}
-      </div>
+          <div className='container'>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/tweet/:id' component={TweetPage} />
+                  <Route path='/new' component={NewTweet} />
+                </div>}
+          </div>
+        </Fragment>
+      </Router>
     )
   }
 }
